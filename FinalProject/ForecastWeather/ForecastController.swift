4e3c64884efa67ba79a5904420792436ca9b2cc3
iuri <ijiki16@freeuni.edu.ge>
2021-02-03 23:54:50 +0400

    add refresh buttons

--- FinalProject/ForecastWeather/ForecastController.swift
+++ FinalProject/ForecastWeather/ForecastController.swift
@@ -16,12 +16,21 @@ class ForecastController: UIViewController {
         self.navigationController!.navigationBar.shadowImage = UIImage()
         self.navigationController!.navigationBar.isTranslucent = true
         self.title = "Forecast"
+        self.navigationController!.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"),
+                                                                style: .plain,
+                                                                target: self,
+                                                                action: #selector(refersh))
         //
         var gradientLayer: CAGradientLayer!
         gradientLayer = CAGradientLayer()
         gradientLayer.frame = self.view.bounds
         gradientLayer.colors = [UIColor(named: "bg-gradient-start")?.cgColor ?? UIColor.blue.cgColor , UIColor(named: "bg-gradient-end")?.cgColor ?? UIColor.red.cgColor]
         self.view.layer.addSublayer(gradientLayer)
+    }
+    
+    @objc func refersh() {
+        print("refresh")
     }
 }
 
--- FinalProject/TodayWeather/TodayController.swift
+++ FinalProject/TodayWeather/TodayController.swift
@@ -16,6 +16,11 @@ class TodayController: UIViewController {
         self.navigationController!.navigationBar.shadowImage = UIImage()
         self.navigationController!.navigationBar.isTranslucent = true
         self.title = "Today"
+        self.navigationController!.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"),
+                                                                style: .plain,
+                                                                target: self,
+                                                                action: #selector(refersh))
         //
         var gradientLayer: CAGradientLayer!
         gradientLayer = CAGradientLayer()
@@ -25,5 +30,8 @@ class TodayController: UIViewController {
         //        self.view.backgroundColor = .red
     }
     
+    @objc func refersh() {
+        print("refresh")
+    }
     
 }
