b28be493e5240748c7dbfd88113f040801dae58f
iuri <ijiki16@freeuni.edu.ge>
2021-02-08 22:19:45 +0400

    fix forecastTable data set

---
+++ FinalProject/ForecastWeather/ForecastTableHeader.swift
@@ -0,0 +1,20 @@
+//
+//  ForecastTableHeader.swift
+//  FinalProject
+//
+//  Created by Iuri Jikidze on 2/8/21.
+//
+
+import UIKit
+
+class ForecastTableHeader: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
